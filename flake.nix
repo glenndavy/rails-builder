62ad9549d0051469a4f12d0601ef7a8bb4196f18
Glenn <glenn@davy.net.au>
2025-04-23 18:21:32 +1200

    fix for missinghomelss shelter

--- flake.nix
+++ flake.nix
@@ -1,56 +1,66 @@
 {
-  description = "Rails app builder flake";
+     description = "Rails app builder flake";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
-  };
+     inputs = {
+       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+       nixpkgs-ruby.url = "github:bobvanderlinden/nixpkgs-ruby";
+     };
 
-  outputs = { self, nixpkgs, nixpkgs-ruby }: let
-    forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system: f system);
-  in {
-    lib = {
-      buildRailsApp = { system, rubyVersion, src, gems }: 
-        let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ nixpkgs-ruby.overlays.default ];
-          };
-          rubyVersionDotted = builtins.replaceStrings ["_"] ["."] rubyVersion;
-          ruby = pkgs."ruby-${rubyVersionDotted}";
-        in
-        pkgs.stdenv.mkDerivation {
-          name = "rails-app";
-          inherit src;
-          buildInputs = [ ruby ];
-          buildPhase = ''
-            bundle install --path vendor/bundle
-            rails assets:precompile
-          '';
-          installPhase = ''
-            mkdir -p $out
-            cp -r . $out
-          '';
-        };
+     outputs = { self, nixpkgs, nixpkgs-ruby }: let
+       forAllSystems = f: nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (system: f system);
+     in {
+       lib = {
+         buildRailsApp = { system, rubyVersion, src, gems }: 
+           let
+             needsInsecureOpenSSL = let
+               versionParts = builtins.splitVersion (builtins.replaceStrings ["_"] ["."] rubyVersion);
+               major = builtins.elemAt versionParts 0;
+               minor = builtins.elemAt versionParts 1;
+             in (major == "2" || (major == "3" && minor == "0") || (major == "3" && minor == "1"));
+             pkgs = import nixpkgs {
+               inherit system;
+               overlays = [ nixpkgs-ruby.overlays.default ];
+               config = {
+                 permittedInsecurePackages = nixpkgs.lib.optionals needsInsecureOpenSSL [ "openssl-1.1.1w" ];
+               };
+             };
+             rubyVersionDotted = builtins.replaceStrings ["_"] ["."] rubyVersion;
+             ruby = pkgs."ruby-${rubyVersionDotted}";
+           in
+           pkgs.stdenv.mkDerivation {
+             name = "rails-app";
+             inherit src;
+             buildInputs = [ ruby ];
+             buildPhase = ''
+               export BUNDLE_USER_HOME=$TMPDIR/bundle
+               mkdir -p $BUNDLE_USER_HOME
+               bundle install --path vendor/bundle
+               rails assets:precompile
+             '';
+             installPhase = ''
+               mkdir -p $out
+               cp -r . $out
+             '';
+           };
 
-      nixosModule = { railsApp, ... }: {
-        imports = [ ./nixos-module.nix ];
-        config.deployment.railsApp = railsApp;
-      };
+         nixosModule = { railsApp, ... }: {
+           imports = [ ./nixos-module.nix ];
+           config.deployment.railsApp = railsApp;
+         };
 
-      dockerImage = { system, railsApp }: 
-        let
-          pkgs = import nixpkgs { inherit system; };
-        in
-        pkgs.dockerTools.buildImage {
-          name = "rails-app";
-          tag = "latest";
-          contents = [ railsApp ];
-          config = {
-            Cmd = [ "${railsApp}/bin/bundle" "exec" "puma" "-C" "config/puma.rb" ];
-            ExposedPorts = { "3000/tcp" = {}; };
-          };
-        };
-    };
-  };
-}
+         dockerImage = { system, railsApp }: 
+           let
+             pkgs = import nixpkgs { inherit system; };
+           in
+           pkgs.dockerTools.buildImage {
+             name = "rails-app";
+             tag = "latest";
+             contents = [ railsApp ];
+             config = {
+               Cmd = [ "${railsApp}/bin/bundle" "exec" "puma" "-C" "config/puma.rb" ];
+               ExposedPorts = { "3000/tcp" = {}; };
+             };
+           };
+       };
+     };
+   }
