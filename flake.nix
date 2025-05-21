b56283c7a1b972c5785df3b6555a0cf0992a1579
Glenn <glenn@davy.net.au>
2025-05-21 13:52:00 +1200

    tried using port number 6579 instead of socket for redis

--- flake.nix
+++ flake.nix
@@ -262,7 +262,7 @@
           export XDG_DATA_DIRS=${effectivePkgs.shared-mime-info}/share:$XDG_DATA_DIRS
           export FREEDESKTOP_MIME_TYPES_PATH=${effectivePkgs.shared-mime-info}/share/mime/packages/freedesktop.org.xml
           export TZDIR=${effectivePkgs.tzdata}/share/zoneinfo
-          #export REDIS_URL=redis://localhost:6379 #/0 # Default Redis URL for runtime
+          export REDIS_URL=redis://localhost:6379 #/0 # Default Redis URL for runtime
           export CC=${gcc}/bin/gcc
           export CXX=${gcc}/bin/g++
           echo "Using GCC version: $(${gcc}/bin/gcc --version | head -n 1)"
@@ -298,11 +298,11 @@
           export REDIS_SOCKET=$TMPDIR/redis.sock
           export REDIS_PID=$TMPDIR/redis.pid
           mkdir -p $TMPDIR
-          ${effectivePkgs.redis}/bin/redis-server --unixsocket $REDIS_SOCKET --pidfile $REDIS_PID --daemonize yes --port 0
+          ${effectivePkgs.redis}/bin/redis-server --unixsocket $REDIS_SOCKET --pidfile $REDIS_PID --daemonize yes --port 6379
           sleep 2
           # Verify Redis is running
           ${effectivePkgs.redis}/bin/redis-cli -s $REDIS_SOCKET ping || { echo "Redis failed to start"; exit 1; }
-          export REDIS_URL="redis://$REDIS_SOCKET"
+          #export REDIS_URL="redis://$REDIS_SOCKET"
 
           echo "\n********************* Setting up bundler ********************************************\n"
 
@@ -634,7 +634,7 @@
           export XDG_DATA_DIRS=${effectivePkgs.shared-mime-info}/share:$XDG_DATA_DIRS
           export FREEDESKTOP_MIME_TYPES_PATH=${effectivePkgs.shared-mime-info}/share/mime/packages/freedesktop.org.xml
           export TZDIR=${effectivePkgs.tzdata}/share/zoneinfo
-          #export REDIS_URL=redis://localhost:6379/0 # Default Redis URL for runtime
+          export REDIS_URL=redis://localhost:6379/0 # Default Redis URL for runtime
           export CC=${gcc}/bin/gcc
           export CXX=${gcc}/bin/g++
           mkdir -p .nix-gems $BUNDLE_PATH/bin $PWD/.bundle
@@ -759,7 +759,7 @@
           echo "LD_LIBRARY_PATH: $LD_LIBRARY_PATH"
           echo "XDG_DATA_DIRS: $XDG_DATA_DIRS"
           echo "FREEDESKTOP_MIME_TYPES_PATH: $FREEDESKTOP_MIME_TYPES_PATH"
-          #export REDIS_URL=redis://localhost:6379/0 # Default Redis URL for runtime
+          export REDIS_URL=redis://localhost:6379 #/0 # Default Redis URL for runtime
           echo "CC: $CC"
           echo "CXX: $CXX"
           echo "Ruby version: ''$(ruby --version)"
