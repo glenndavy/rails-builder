6ce73e719b76f479f22af125031819e023fe846b
Glenn <glenn@davy.net.au>
2025-06-10 11:15:32 +1200

    bump to version 2.0.56 - remove added errors again

--- templates/new-app/flake.nix
+++ templates/new-app/flake.nix
@@ -19,8 +19,8 @@
     ...
   }: let
     system = "x86_64-linux";
-    overlays = [ nixpkgs-ruby.overlays.default ];
-    pkgs = import nixpkgs { inherit system overlays; };
+    overlays = [nixpkgs-ruby.overlays.default];
+    pkgs = import nixpkgs {inherit system overlays;};
     version = "2.0.56"; # Frontend version
 
     # Detect Ruby version
@@ -93,7 +93,7 @@
     };
 
     # Call backend builder with source including build artifacts
-    railsBuild = rails-builder.lib.mkRailsBuild (buildConfig // { src = ./.; });
+    railsBuild = rails-builder.lib.mkRailsBuild (buildConfig // {src = ./.;});
     rubyPackage = pkgs."ruby-${rubyVersion}";
     bundlerPackage = pkgs.bundler;
   in {
@@ -136,7 +136,7 @@
         cat ${pkgs.writeText "flake-version" ''
           Frontend Flake Version: ${version}
           Backend Flake Version: ${rails-builder.lib.version or "2.0.25"}
-        '')}
+        ''}
       '';
       manage-postgres = pkgs.writeShellScriptBin "manage-postgres" ''
         #!${pkgs.runtimeShell}
