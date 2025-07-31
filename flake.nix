eb623a17bd02c3b20f129d4a35a5f83167fb6014
Glenn <glenn@davy.net.au>
2025-07-31 21:46:59 +1200

    change how ultraman is called

--- flake.nix
+++ flake.nix
@@ -8,24 +8,18 @@
     ultraman.url = "github:yukihirop/ultraman";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    nixpkgs-ruby,
-    ultraman,
-    ...
-  }: let
+  outputs = { self, nixpkgs, nixpkgs-ruby, ultraman }: let
     system = "x86_64-linux";
-    version = "2.0.26"; # Backend version
+    version = "2.0.25"; # Backend version
     overlays = [nixpkgs-ruby.overlays.default];
-    pkgs = import nixpkgs {inherit system overlays;};
+    pkgs = import nixpkgs { inherit system overlays; };
 
     # Function to create build environment
     mkRailsBuild = {
         rubyVersion,
         gccVersion ? "latest",
         opensslVersion ? "3_2",
-        src ? ./.
+        src ? ./.,
       }: let
         rubyPackage = pkgs."ruby-${rubyVersion}";
         gccPackage =
@@ -67,41 +61,40 @@
             pkgs.rsync
             pkgs.nodejs
           ];
-        shellHook = ''
-          export PKG_CONFIG_PATH="${pkgs.curl.dev}/lib/pkgconfig:${pkgs.postgresql}/lib/pkgconfig"
-          export LD_LIBRARY_PATH="${pkgs.curl}/lib:${pkgs.postgresql}/lib:${opensslPackage}/lib"
-          export TZDIR="$HOME/zoneinfo"
-          mkdir -p "$HOME/zoneinfo"
-          ln -sf "${pkgs.tzdata}/share/zoneinfo" "$HOME/zoneinfo"
-        '';
-      };
-      inherit app;
-      dockerImage = pkgs.dockerTools.buildLayeredImage {
-        name = "rails-app";
-        tag = "latest";
-        contents = [
-          app 
-          ultraman.packages.${system}.ultraman
-          rubyPackage 
-          pkgs.curl 
-          opensslPackage 
-          pkgs.postgresql 
-          pkgs.rsync 
-          pkgs.tzdata 
-          pkgs.zlib 
-          pkgs.gosu 
-          pkgs.nodejs 
-          pkgs.libyaml 
-          pkgs.bash
-        ];
-        config = {
-          #Cmd = ["${rubyPackage}/bin/ruby" "${app}/bin/rails" "server" "-b" "0.0.0.0"];
-          Cmd = ["${ultraman.packages.${system}.ultraman}/bin/ultraman start web"];
-          Env = ["BUNDLE_PATH=/vendor/bundle" "RAILS_ENV=production"];
-          ExposedPorts = {"3000/tcp" = {};};
+          shellHook = ''
+            export PKG_CONFIG_PATH="${pkgs.curl.dev}/lib/pkgconfig:${pkgs.postgresql}/lib/pkgconfig"
+            export LD_LIBRARY_PATH="${pkgs.curl}/lib:${pkgs.postgresql}/lib:${opensslPackage}/lib"
+            export TZDIR="$HOME/zoneinfo"
+            mkdir -p "$HOME/zoneinfo"
+            ln -sf "${pkgs.tzdata}/share/zoneinfo" "$HOME/zoneinfo"
+          '';
+        };
+        inherit app;
+        dockerImage = pkgs.dockerTools.buildLayeredImage {
+          name = "rails-app";
+          tag = "latest";
+          contents = [
+            app
+            ultraman.packages.${system}.default # Use .default as per ultraman flake
+            rubyPackage
+            pkgs.curl
+            opensslPackage
+            pkgs.postgresql
+            pkgs.rsync
+            pkgs.tzdata
+            pkgs.zlib
+            pkgs.gosu
+            pkgs.nodejs
+            pkgs.libyaml
+            pkgs.bash
+          ];
+          config = {
+            Cmd = [ "${ultraman.packages.${system}.default}/bin/ultraman" "start" "web" ];
+            Env = [ "BUNDLE_PATH=/vendor/bundle" "RAILS_ENV=production" ];
+            ExposedPorts = { "3000/tcp" = {}; };
+          };
         };
       };
-    };
   in {
     lib = {
       inherit mkRailsBuild;
