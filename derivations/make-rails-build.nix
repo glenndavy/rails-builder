dfc926c84df67ef1339283502861491c35d9fe5c
Glenn <glenn@davy.net.au>
2025-08-05 13:23:05 +1200

    trying different indentation

--- derivations/make-rails-build.nix
+++ derivations/make-rails-build.nix
@@ -22,13 +22,13 @@
       buildInputs = [pkgs.coreutils];
       dontUnpack = true;
       installPhase = ''
-        echo "DEBUG: usrBinDerviation install phase" >&2
-        echo "DEBUG: Creating usr/bin/env symlink" >&2
-        mkdir -p $out/usr/bin
-        ln -sf ${pkgs.coreutils}/bin/env $out/usr/bin/env
-        echo "DEBUG: Contents of $out/usr/bin:" >&2
-        ls -l $out/usr/bin >&2
-        echo "DEBUG: usrBinDerivation completed" >&2
+      echo "DEBUG: usrBinDerviation install phase" >&2
+      echo "DEBUG: Creating usr/bin/env symlink" >&2
+      mkdir -p $out/usr/bin
+      ln -sf ${pkgs.coreutils}/bin/env $out/usr/bin/env
+      echo "DEBUG: Contents of $out/usr/bin:" >&2
+      ls -l $out/usr/bin >&2
+      echo "DEBUG: usrBinDerivation completed" >&2
       '';
     };
     app = pkgs.stdenv.mkDerivation {
@@ -46,50 +46,50 @@
         pkgs.libyaml
       ];
       buildPhase = ''
-        echo "DEBUG: rails-app build phase start" >&2
-        export HOME=$PWD
-        export source=$PWD
-        # remove - as this should not be in a sandbox:# echo "DEBUG: Running build-rails-app in buildPhase" >&2
-        # remove - as this should not be in a sandbox:#${buildRailsApp}/bin/build-rails-app
-        # remove - as this should not be in a sandbox:#echo "DEBUG: Contents of vendor/bundle after build-rails-app:" >&2
-        # ls -lR vendor/bundle >&2
-        echo "DEBUG: rails-app build phase done" >&2
+      echo "DEBUG: rails-app build phase start" >&2
+      export HOME=$PWD
+      export source=$PWD
+      # remove - as this should not be in a sandbox:# echo "DEBUG: Running build-rails-app in buildPhase" >&2
+      # remove - as this should not be in a sandbox:#${buildRailsApp}/bin/build-rails-app
+      # remove - as this should not be in a sandbox:#echo "DEBUG: Contents of vendor/bundle after build-rails-app:" >&2
+      # ls -lR vendor/bundle >&2
+      echo "DEBUG: rails-app build phase done" >&2
       '';
       installPhase = ''
-        echo "DEBUG: rails-app install phase start" >&2
-        echo "DEBUG: PWD: $(pwd)" >&2
-        echo "DEBUG: Source directory contents:" >&2
-        ls -l >&2
-        ls -lR .|wc -l >&2
-        mkdir -p $out/app
-        cp -r ./* $out/app
-        if [ -d "vendor/bundle" ]; then
-          echo "DEBUG: Copying vendor/bundle to $out/app/vendor/bundle" >&2
-          rsync -a --delete "vendor/bundle/" "$out/app/vendor/bundle/"
-          chmod -R u+w $out/app/vendor/bundle
-          echo "DEBUG: Contents of $out/app/vendor/bundle:" >&2
-          #ls -lR $out/app/vendor/bundle >&2
-          [ -f "$out/app/vendor/bundle/bin/bundler" ] && echo "DEBUG: bundler executable found" >&2 || echo "ERROR: bundler executable missing" >&2
-          [ -f "$out/app/vendor/bundle/bin/rails" ] && echo "DEBUG: rails executable found" >&2 || echo "ERROR: rails executable missing" >&2
-        else
-          echo "ERROR: No vendor/bundle found" >&2
-          exit 1
-        fi
-        if [ -d "public/packs" ]; then
-          rsync -a --delete "public/packs/" "$out/app/public/packs/"
-          echo "DEBUG: Contents of $out/app/public/packs:" >&2
-          ls -l $out/app/public/packs >&2
-        fi
-        echo "DEBUG: Setting up additional filesystem paths" >&2
-        mkdir -p $out/root/zoneinfo
-        ln -sf ${pkgs.tzdata}/share/zoneinfo $out/root/zoneinfo
-        mkdir -p $out/app/.nix-gems
-        mkdir -p $out/usr/local/bin
-        ln -sf ${rubyPackage}/bin/* $out/usr/local/bin/
-        echo "DEBUG: Contents of $out/usr/local/bin:" >&2
-        ls -l $out/usr/local/bin >&2
-        echo "DEBUG: Filesystem setup completed" >&2
-        echo "DEBUG: rails-app install phase done" >&2
+      echo "DEBUG: rails-app install phase start" >&2
+      echo "DEBUG: PWD: $(pwd)" >&2
+      echo "DEBUG: Source directory contents:" >&2
+      ls -l >&2
+      ls -lR .|wc -l >&2
+      mkdir -p $out/app
+      cp -r ./* $out/app
+      if [ -d "vendor/bundle" ]; then
+        echo "DEBUG: Copying vendor/bundle to $out/app/vendor/bundle" >&2
+        rsync -a --delete "vendor/bundle/" "$out/app/vendor/bundle/"
+        chmod -R u+w $out/app/vendor/bundle
+        echo "DEBUG: Contents of $out/app/vendor/bundle:" >&2
+        #ls -lR $out/app/vendor/bundle >&2
+        [ -f "$out/app/vendor/bundle/bin/bundler" ] && echo "DEBUG: bundler executable found" >&2 || echo "ERROR: bundler executable missing" >&2
+        [ -f "$out/app/vendor/bundle/bin/rails" ] && echo "DEBUG: rails executable found" >&2 || echo "ERROR: rails executable missing" >&2
+      else
+        echo "ERROR: No vendor/bundle found" >&2
+        exit 1
+      fi
+      if [ -d "public/packs" ]; then
+        rsync -a --delete "public/packs/" "$out/app/public/packs/"
+        echo "DEBUG: Contents of $out/app/public/packs:" >&2
+        ls -l $out/app/public/packs >&2
+      fi
+      echo "DEBUG: Setting up additional filesystem paths" >&2
+      mkdir -p $out/root/zoneinfo
+      ln -sf ${pkgs.tzdata}/share/zoneinfo $out/root/zoneinfo
+      mkdir -p $out/app/.nix-gems
+      mkdir -p $out/usr/local/bin
+      ln -sf ${rubyPackage}/bin/* $out/usr/local/bin/
+      echo "DEBUG: Contents of $out/usr/local/bin:" >&2
+      ls -l $out/usr/local/bin >&2
+      echo "DEBUG: Filesystem setup completed" >&2
+      echo "DEBUG: rails-app install phase done" >&2
       '';
     };
     shell = pkgs.mkShell {
@@ -108,14 +108,14 @@
         pkgs.nodejs
       ];
       shellHook = ''
-        echo "DEBUG: Shell hook for  shell " >&2
-        export PS1="shell:>"
-        export PKG_CONFIG_PATH="${pkgs.curl.dev}/lib/pkgconfig:${pkgs.postgresql}/lib/pkgconfig"
-        export LD_LIBRARY_PATH="${pkgs.curl}/lib:${pkgs.postgresql}/lib:${opensslPackage}/lib"
-        export TZDIR="$HOME/zoneinfo"
-        mkdir -p "$HOME/zoneinfo"
-        ln -sf "${pkgs.tzdata}/share/zoneinfo" "$HOME/zoneinfo"'
-        echo "DEBUG: shell hook done" >&2
+      echo "DEBUG: Shell hook for  shell " >&2
+      export PS1="shell:>"
+      export PKG_CONFIG_PATH="${pkgs.curl.dev}/lib/pkgconfig:${pkgs.postgresql}/lib/pkgconfig"
+      export LD_LIBRARY_PATH="${pkgs.curl}/lib:${pkgs.postgresql}/lib:${opensslPackage}/lib"
+      export TZDIR="$HOME/zoneinfo"
+      mkdir -p "$HOME/zoneinfo"
+      ln -sf "${pkgs.tzdata}/share/zoneinfo" "$HOME/zoneinfo"'
+      echo "DEBUG: shell hook done" >&2
      '';
     };
   in {
@@ -163,40 +163,40 @@
         '';
         enableFakechroot = true;
         fakeRootCommands = ''
-          set -x
-					mkdir -p /etc
-					cat > /etc/passwd <<EOF
-					root:x:0:0::/root:/bin/bash
-					app_user:x:1000:1000:App User:/app:/bin/bash
-					EOF
-					cat > /etc/group <<EOF
-					root:x:0:
-					app_user:x:1000:
-					EOF
-					# Optional shadow
-					cat > /etc/shadow <<EOF
-					root:*:18000:0:99999:7:::
-					app_user:*:18000:0:99999:7:::
-					EOF
-					chown -R 1000:1000 /app
-					chmod -R u+w /app
-					echo "DEBUG: Contents of /etc:" >&2
-					ls -l /etc >&2
+        set -x
+				mkdir -p /etc
+				cat > /etc/passwd <<-EOF
+				root:x:0:0::/root:/bin/bash
+				app_user:x:1000:1000:App User:/app:/bin/bash
+				EOF
+				cat > /etc/group <<-EOF
+				root:x:0:
+				app_user:x:1000:
+				EOF
+				# Optional shadow
+				cat > /etc/shadow <<-EOF
+				root:*:18000:0:99999:7:::
+				app_user:*:18000:0:99999:7:::
+				EOF
+				chown -R 1000:1000 /app
+				chmod -R u+w /app
+				echo "DEBUG: Contents of /etc:" >&2
+				ls -l /etc >&2
        '';
         
         #extraCommands = ''
         #  echo "DEBUG: Starting extraCommands" >&2
         #  mkdir -p etc
-        #  cat > etc/passwd <<EOF
+        #  cat > etc/passwd <<-EOF
         #  root:x:0:0::/root:/bin/bash
         #  app_user:x:1000:1000:App User:/app:/bin/bash
         #  EOF
-        #  cat > etc/group <<EOF
+        #  cat > etc/group <<-EOF
         #  root:x:0:
         #  app_user:x:1000:
         #  EOF
         #  # Optional shadow (for no password)
-        #  cat > etc/shadow <<EOF
+        #  cat > etc/shadow <<-EOF
         #  root:*:18000:0:99999:7:::
         #  app_user:*:18000:0:99999:7:::
         #  EOF
