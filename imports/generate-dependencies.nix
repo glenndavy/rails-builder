76b055f3919cf5c0654701679fd34b7f2026cbfd
Glenn <glenn@davy.net.au>
2025-08-19 19:07:47 +1200

    fixing another generate dependencies issue

--- imports/generate-dependencies.nix
+++ imports/generate-dependencies.nix
@@ -6,13 +6,14 @@
   #!${pkgs.runtimeShell}
   set -e
   echo "Generating gemset.nix from Gemfile.lock..."
-  export PATH=${rubyPackage}/bin:$PATH
+  export PATH=${rubyPackage}/bin:${pkgs.nix-prefetch-git}/bin:$PATH
   echo "Verifying Ruby version: $(${rubyPackage}/bin/ruby -v)"
   export GEM_HOME=$(mktemp -d)
+  rm -rf vendor/bundle .bundle  # Clean potential interfering dirs
   ${rubyPackage}/bin/gem install bundler --version ${bundlerVersion} --no-document
   ${rubyPackage}/bin/gem install bundix --no-document
   export PATH=$GEM_HOME/bin:$PATH
-  bundix -l  # Lock mode: Parses Gemfile.lock, no install needed
+  bundix -l  # Lock mode
   if [ -f yarn.lock ]; then
     echo "Computing Yarn hash..."
     YARN_HASH=$(${pkgs.prefetch-yarn-deps}/bin/prefetch-yarn-deps yarn.lock | grep sha256 | cut -d '"' -f2)
