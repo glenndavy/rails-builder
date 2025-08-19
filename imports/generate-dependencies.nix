b9bfe10188d109eb41fbd1706612309acd0a5ffc
Glenn <glenn@davy.net.au>
2025-08-19 13:01:18 +1200

    fix generate depenceis.nix

--- imports/generate-dependencies.nix
+++ imports/generate-dependencies.nix
@@ -1,11 +1,22 @@
-{pkgs}: ''
+{
+  pkgs,
+  rubyPackage,
+  bundlerVersion,
+}: ''
   #!${pkgs.runtimeShell}
-  echo "Generating prefetch files..."
-  nix-shell -p bundix --run "bundix --magic"  # Or bundix -l for lock
-  git add gemset.nix
-  echo "Computing Yarn hash..."
-  YARN_HASH=$(nix run nixpkgs#prefetch-yarn-deps -- yarn.lock | grep sha256 | sed -E 's/.*"([^"]+)".*/\1/')
-  echo "Updating Nix expr with sha256=$YARN_HASH"
-  sed -i "s|sha256 = \".*\";|sha256 = \"$YARN_HASH\";|" flake.nix  # Adjust path/pattern as needed
-  echo "DONE"
+  set -e
+  echo "Generating gemset.nix with Bundler ${bundlerVersion} on Ruby ${rubyPackage.version}..."
+  export PATH=${rubyPackage}/bin:$PATH
+  export GEM_HOME=$(mktemp -d)
+  gem install bundler --version ${bundlerVersion} --no-document
+  bundle install --path vendor/bundle --standalone
+  ${pkgs.bundix}/bin/bundix --magic  # Or bundix -l for lock-only
+  if [ -f yarn.lock ]; then
+    echo "Computing Yarn hash..."
+    YARN_HASH=$(${pkgs.prefetch-yarn-deps}/bin/prefetch-yarn-deps yarn.lock)
+    echo "Yarn hash (for fetchYarnDeps sha256): $YARN_HASH"
+    # Optionally sed into flake.nix: sed -i "s|sha256 = \".*\";|sha256 = \"$YARN_HASH\";|" flake.nix
+  fi
+  git add gemset.nix  # Add yarn.hash if saving separately
+  echo "Done."
 ''
