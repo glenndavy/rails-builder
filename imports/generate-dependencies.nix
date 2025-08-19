ff61e6f8b7aef4a45a871d22d0519e457b85c06d
Glenn <glenn@davy.net.au>
2025-08-19 14:34:17 +1200

    attempt to ensure correct ruby package

--- imports/generate-dependencies.nix
+++ imports/generate-dependencies.nix
@@ -7,13 +7,15 @@
   set -e
   echo "Generating gemset.nix with Bundler ${bundlerVersion} on Ruby ${rubyPackage.version}..."
   export PATH=${rubyPackage}/bin:$PATH
+  echo "Verifying Ruby version: $(${rubyPackage}/bin/ruby -v)"
   export GEM_HOME=$(mktemp -d)
-  gem install bundler --version ${bundlerVersion} --no-document
-  bundle install --path vendor/bundle --standalone
+  ${rubyPackage}/bin/gem install bundler --version ${bundlerVersion} --no-document
+  export PATH=$GEM_HOME/bin:$PATH
+  ${rubyPackage}/bin/bundle install --path vendor/bundle --standalone
   ${pkgs.bundix}/bin/bundix --magic  # Or bundix -l for lock-only
   if [ -f yarn.lock ]; then
     echo "Computing Yarn hash..."
-    YARN_HASH=$(${pkgs.prefetch-yarn-deps}/bin/prefetch-yarn-deps yarn.lock)
+    YARN_HASH=$(${pkgs.prefetch-yarn-deps}/bin/prefetch-yarn-deps yarn.lock | grep sha256 | cut -d '"' -f2)
     echo "Yarn hash (for fetchYarnDeps sha256): $YARN_HASH"
     # Optionally sed into flake.nix: sed -i "s|sha256 = \".*\";|sha256 = \"$YARN_HASH\";|" flake.nix
   fi
