72525bd20413a681a245da580a32373ca14f23a5
Glenn <glenn@davy.net.au>
2025-08-20 08:15:59 +1200

    experimenting with making build more pure by removing known stdlibs

--- imports/generate-dependencies.nix
+++ imports/generate-dependencies.nix
@@ -6,14 +6,17 @@
   #!${pkgs.runtimeShell}
   set -e
   echo "Generating gemset.nix from Gemfile.lock..."
-  export PATH=${rubyPackage}/bin:${pkgs.nix-prefetch-git}/bin:$PATH
+  export PATH=${rubyPackage}/bin:${pkgs.nix-prefetch-scripts}/bin:$PATH
   echo "Verifying Ruby version: $(${rubyPackage}/bin/ruby -v)"
   export GEM_HOME=$(mktemp -d)
-  rm -rf vendor/bundle .bundle  # Clean potential interfering dirs
   ${rubyPackage}/bin/gem install bundler --version ${bundlerVersion} --no-document
   ${rubyPackage}/bin/gem install bundix --no-document
   export PATH=$GEM_HOME/bin:$PATH
-  bundix -l  # Lock mode
+  bundix -l
+  # Remove bundled stdlib gems causing nil errors
+  sed -i '/"net-pop"/,/^    };/d' gemset.nix
+  sed -i '/"matrix"/,/^    };/d' gemset.nix
+  # Add more sed for other bundled if needed (e.g., net-smtp, prime from Ruby 3.2 list)
   if [ -f yarn.lock ]; then
     echo "Computing Yarn hash..."
     YARN_HASH=$(${pkgs.prefetch-yarn-deps}/bin/prefetch-yarn-deps yarn.lock | grep sha256 | cut -d '"' -f2)
